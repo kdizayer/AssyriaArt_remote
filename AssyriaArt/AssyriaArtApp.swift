c5825cd3ffccad0da1eef0bb9098fd84b41bee99
kdizayer <kdizayer@gmail.com>
2024-02-17 13:45:20 -0800

    Initial Commit

---
+++ AssyriaArt/AssyriaArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssyriaArtApp.swift
+//  AssyriaArt
+//
+//  Created by Kayla Dizayer on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AssyriaArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
