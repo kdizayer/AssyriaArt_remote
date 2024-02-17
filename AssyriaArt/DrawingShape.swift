4959b37e860aedd7e74148ae5582bd128cb65ced
kdizayer <kdizayer@gmail.com>
2024-02-17 14:47:17 -0800

    Clean up code

--- AssyriaArt/DrawingShape.swift
+++ AssyriaArt/DrawingShape.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct DrawingShape: Shape {
+    let points: [CGPoint]
+    let engine = DrawingEngine()
+    func path(in rect: CGRect) -> Path {
+        engine.createPath(for: points)
+    }
+}
--- AssyriaArt/RootView.swift
+++ AssyriaArt/RootView.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct RootView: View {
+    @State private var showTabView = false
+
+    var body: some View {
+        if showTabView {
+            MainTabView() // Your tab view
+        } else {
+            ZStack {
+                // Background image
+                Image("flag")
+                   // .resizable()
+                    .edgesIgnoringSafeArea(.all) // Make sure the image fills the whole screen
+                    .aspectRatio(contentMode: .fill) // Fill the screen while preserving aspect ratio
+
+                
+                VStack {
+                    Text("Welcome to the Drawing App!")
+                        .font(.title)
+                        .padding()
+                        .foregroundColor(.black) // Ensure text is visible on the background
+
+                    Button("Start") {
+                        showTabView = true
+                    }
+                    .padding()
+                    .background(Color.yellow)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .frame(width: 5000, height: 5000) // button size
+                    .offset(x: 0, y: -50)
+                }
+            }
+        }
+    }
+}
